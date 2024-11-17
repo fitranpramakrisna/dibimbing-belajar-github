c2414ccfe6b91d91f41718a27d67564e3f9ae6be
Fitran Dwi Pramakrsina <fitranpramakrisna@gmail.com>
2024-11-17 22:31:49 +0700

    add python file to count rows of csv table

---
+++ calculate_row.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def read_csv(path):
+    df = pd.read_csv(path, sep = ';')
+    return df
+
+def calculate_row(df):
+    df = read_csv(path)
+    row_len = len(df)
+    return row_len
+
+path = r'file/username.csv'
+
+print(calculate_row(read_csv(path)))
